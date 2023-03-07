9dab8bcbc1690e79d12676acc886340da6d42f13
Nazmos27 <nazmos789@gmail.com>
2023-03-08 02:31:39 +0600

    modal activated,showing details

--- ai_hub.js
+++ ai_hub.js
@@ -101,7 +101,73 @@ document.getElementById('see-more-btn').addEventListener('click', function () {
 
 
 
+const displayDetails = data =>{
+  let features ="" ;
+  let array = Object.values(data.data.features);
+  if(data.data.features != null){
+  for(let i=0; i<array.length;i++){
+    features += `<li style="font-size:10px" class="fw-7 color-primary">${array[i].feature_name}</li>`;
+  }
+}
+else{
+  features="No data Found";
+}
+  let Integrations ="" ;
+  if(data.data.integrations != null){
+    for(let i=0; i<data.data.integrations.length;i++){
+      Integrations += `<li style="font-size:10px" class="align-right p-0 color-primary">${data.data.integrations[i]}</li>`;
+    }
+  }
+  else{
+    Integrations='No data Found';
+  }
+ 
+   
+ 
+  // for card 1 select
+  const card1Title = document.getElementById('card1-title');
+  const pricing = document.getElementById('pricing');
+  const cardFooter = document.getElementById('card-footer');
+ 
 
+  // for card 2  select
+
+  const modalImage = document.getElementById('modalImage');
+  const card2Title = document.getElementById('card-title');
+  const cardText = document.getElementById('card-text');
+  // for card 1 
+  card1Title.innerText=`${data.data.description}`;
+  pricing.innerHTML=`
+  <div  class=" d-flex flex-column flex-sm-row align-center priceParent  mt-3 align-items-stretch text-center  align-items-center justify-content-center gap-4">
+  <div style="font-size:10px;" class="price bg-body p-3 rounded  w-100  text-success">${data.data.pricing!=null ? data.data.pricing[0].price : 'Free Of'}<br>${data.data.pricing!=null ? data.data.pricing[0].plan :''}</div>
+  <div style="font-size:10px;" class="price bg-body p-3 rounded   w-100 text-warning"> ${data.data.pricing!=null  ? data.data.pricing[1].price:'Free Of'}<br>${data.data.pricing!=null ?data.data.pricing[1].plan: ''}</div>
+  <div style="font-size:10px;" class="price bg-body p-3 rounded w-100 text-danger ">${data.data.pricing!=null ? data.data.pricing[2].price.slice(0,10) :'Free Of'}<br>${data.data.pricing!=null ? data.data.pricing[2].plan :''}</div>
+</div>
+  `;
+  // data.data.
+  cardFooter.innerHTML=`
+  <div  class="d-flex mt-2 justify-content-between">
+  <div  <p>Features</p>
+  <ul class="px-3 " >${features}</ul>
+  </div>
+  <div  class="mx-6"><p>Integrations</p>
+  <ul class="px-3" >${Integrations }</ul>
+  
+  </div>
+</div>
+  `
+
+  // for card 2 
+  modalImage.innerHTML=`
+  <img class="card-img-top rounded-xl img-fluid" src="${data.data.image_link[0]}" class="card-img-top" alt="...">
+  <div class="position-absolute top-0 end-0">
+  ${data.data.accuracy.score ?`<div id="priceScore" class="bg-danger rounded p-1 m-2 border-0 text-white fs-6">${data.data.accuracy.score ? data.data.accuracy.score * 100 : '' }% accuracy</div>`:''}
+</div>
+  `;
+  card2Title.innerText=`${data.data.input_output_examples != null ? data.data.input_output_examples[0].input: 'Can you give any example?'}`;
+  cardText.innerText=`${data.data.input_output_examples != null ? data.data.input_output_examples[0].output : 'No! Not Yet! Take a break!!!' }`
+ 
+}
 
 
 
